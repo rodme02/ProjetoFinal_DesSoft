da02c3ccf2de8ec0321f21a5bf35aa1de5062fb9
PedroSAC <pedrosac@al.insper.edu.br>
2020-06-15 17:59:57 -0300

    Resolvido o bug de transparência

--- sprites.py
+++ sprites.py
@@ -51,7 +51,7 @@ def load_spritesheet(spritesheet, rows, columns):
             # Define o retângulo que contém o sprite atual
             dest_rect = pygame.Rect(x, y, sprite_width, sprite_height)
             # Cria uma imagem vazia do tamanho do sprite
-            image = pygame.Surface((sprite_width, sprite_height))
+            image = pygame.Surface((sprite_width, sprite_height), pygame.SRCALPHA)
             # Copia o sprite atual (do spritesheet) na imagem
             image.blit(spritesheet, (0, 0), dest_rect)
             sprites.append(image)
